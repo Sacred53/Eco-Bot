6db1675c8e1cd9be80cd87daf009b80f40106484
Sacred53 <42985181+Sacred53@users.noreply.github.com>
2018-09-04 19:50:33 -0400

    Add files via upload

---
+++ commands/donttouch.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const errorm = require("./errorm.json");
+
+const bl = require("./list.json");
+module.exports.run = async (bot, message, args) => {
+}
+
+module.exports.help = {
+    name: ""
+};
